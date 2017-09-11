587ba09cf27ede7d08a22491094bd6cf10207014
Patsy Pineda <patsyp014@gmail.com>
2017-09-11 02:35:22 -0500

    finished DojoMail

--- angular_basics/dojoMail/src/app/app.component.ts
+++ angular_basics/dojoMail/src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	//Create a variable emails that is an array of emails.
   emails = [
-  {email:'patsy@gmail.com', important: true, subject: 'nothing', content: 'This is nothing.'},
-  {email: 'email@email.com', important:true, subject: 'something',content:'This is something'},
-  {email: 'hello@mail.com', important: false, subject: 'hi', content:'hello, whats up'},
-  {email: 'bye@email.com', important:false, subject:'goodbye', content:'yo, peace out'}
-  ]
+	  {email:'patsy@gmail.com', important: true, subject: 'nothing', content: 'This is nothing.'},
+	  {email: 'email@email.com', important:true, subject: 'something',content:'This is something'},
+	  {email: 'hello@mail.com', important: false, subject: 'hi', content:'hello, whats up'},
+	  {email: 'bye@email.com', important:false, subject:'goodbye', content:'yo, peace out'}
+	  ]
 }
